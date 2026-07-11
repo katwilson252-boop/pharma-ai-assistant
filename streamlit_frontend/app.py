3ee94cdaae926b15f3af511f84ff38e0c0b49e9a
Iram <katwilson252@gmail.com>
2026-07-12 03:39:06 +0530

    Use deployed backend API

--- streamlit_frontend/app.py
+++ streamlit_frontend/app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import requests
 
-API_URL = "http://127.0.0.1:8000"
+API_URL = "API_URL = "https://pharma-ai-assistant-api.onrender.com""
 
 st.set_page_config(
     page_title="AI-First CRM",
